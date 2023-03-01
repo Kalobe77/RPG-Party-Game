b6d7de18b3d60e884067fa0a78672e1ebb83a73a
Kalobe77 <kgearinger@muhlenberg.edu>
2023-03-01 13:30:29 -0500

    Update Files for newest version of Unity

--- RPG Party Game/Assets/Scripts/InputScript.cs
+++ RPG Party Game/Assets/Scripts/InputScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class InputScript : MonoBehaviour
 {
+
+    public Rigidbody2D rb;
+    public float movementSpeed;
+    
     // Start is called before the first frame update
     void Start()
     {
