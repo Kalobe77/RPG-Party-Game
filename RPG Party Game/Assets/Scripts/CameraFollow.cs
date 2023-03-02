e973dbecbe1604d35515c539f1148e054ba2d653
astefan1644 <90490226+astefan1644@users.noreply.github.com>
2023-03-01 23:53:59 -0500

    Simple background
    Created a simple background and had the camera follow the player.

---
+++ RPG Party Game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float damping;
+
+    private Vector3 velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 movePosition = target.position + offset; 
+        transform.position = Vector3.SmoothDamp(transform.position, movePosition, ref velocity, damping);
+    }
+}
