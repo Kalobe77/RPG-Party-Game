7a863df27e1e666b0476db507738c78bb5068444
Kalobe77 <kgearinger@muhlenberg.edu>
2023-03-08 20:19:00 -0500

    Added Movement Mechanism and Began Adding Dice Sprite

--- RPG Party Game/Assets/Scripts/InputScript.cs
+++ RPG Party Game/Assets/Scripts/InputScript.cs
@@ -11,23 +11,131 @@ public class InputScript : MonoBehaviour
 
     public float movementSpeed;
     Vector2 movement;
+    int node = 0;
+    bool isMoving = false;
+    public float characterOffset = .7f;
 
-    
+    public Route routeScript;
+
     // Update is called once per frame
     void Update()
     {
         // Input
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-
+        if (!isMoving)
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+            StartCoroutine(Move());
+        }
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
-    void FixedUpdate()
+    IEnumerator Move()
     {
-        // Movement
-        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+        if (isMoving)
+        {
+            yield break;
+        }
+        isMoving = true;
+        if (movement.x > 0)
+        {
+            for (int i = 0; i < routeScript.childNodeList.Count; i++)
+            {
+                if (routeScript.routeMatrix[node,i] == 1)
+                {
+                    if (routeScript.childNodeList[i].position.x > routeScript.childNodeList[node].position.x)
+                    {
+                        movement.x = 1;
+                        movement.y = 0;
+                        while(MoveToSquare(routeScript.childNodeList[i].position)){yield return null;}
+                        yield return new WaitForSeconds(0.1f);
+                        node = i;
+                        break;
+                    }
+                    else
+                    {
+                        movement.x = 0;
+                        movement.y = 0;
+                    }
+                }
+            }
+        }
+        else if (movement.x < 0)
+        {
+            for (int i = 0; i < routeScript.childNodeList.Count; i++)
+            {
+                if (routeScript.routeMatrix[node,i] == 1)
+                {
+                    if (routeScript.childNodeList[i].position.x < routeScript.childNodeList[node].position.x)
+                    {
+                        movement.x = -1;
+                        movement.y = 0;
+                        while(MoveToSquare(routeScript.childNodeList[i].position)){yield return null;}
+                        yield return new WaitForSeconds(0.1f);
+                        node = i;
+                        break;
+                    }
+                    else
+                    {
+                        movement.x = 0;
+                        movement.y = 0;
+                    }
+                }
+            }
+        }
+        else if (movement.y > 0)
+        {
+            for (int i = 0; i < routeScript.childNodeList.Count; i++)
+            {
+                if (routeScript.routeMatrix[node,i] == 1)
+                {
+                    if (routeScript.childNodeList[i].position.y > routeScript.childNodeList[node].position.y)
+                    {
+                        movement.x = 0;
+                        movement.y = 1;
+                        while(MoveToSquare(routeScript.childNodeList[i].position)){yield return null;}
+                        yield return new WaitForSeconds(0.1f);
+                        node = i;
+                        break;
+                    }
+                    else
+                    {
+                        movement.x = 0;
+                        movement.y = 0;
+                    }
+                }
+            }
+        }
+        else if (movement.y < 0)
+        {
+            for (int i = 0; i < routeScript.childNodeList.Count; i++)
+            {
+                if (routeScript.routeMatrix[node,i] == 1)
+                {
+                    if (routeScript.childNodeList[i].position.y < routeScript.childNodeList[node].position.y)
+                    {
+                        movement.x = 0;
+                        movement.y = -1;
+                        while(MoveToSquare(routeScript.childNodeList[i].position)){yield return null;}
+                        yield return new WaitForSeconds(0.1f);
+                        node = i;
+                        break;
+                    }
+                    else
+                    {
+                        movement.x = 0;
+                        movement.y = 0;
+                    }
+                }
+            }
+        }
+        isMoving = false;
+    }
+
+    bool MoveToSquare(Vector3 goal)
+    {
+        return new Vector3(goal.x, goal.y+characterOffset, goal.z) != (this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(goal.x, goal.y+characterOffset, goal.z), 5f*Time.deltaTime));
     }
 }
