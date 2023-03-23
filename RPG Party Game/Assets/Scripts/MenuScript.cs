44b76c276f9218bad3184192c8cdeb25d0f11224
astefan1644 <90490226+astefan1644@users.noreply.github.com>
2023-03-23 14:08:33 -0400

    Added Title Screen
    Title, amount of players, load or new game

---
+++ RPG Party Game/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    // Title Screen
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("NewLoad Game");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+///////////////
+    // New/Load Game Screen
+    public void Players()
+    {
+        SceneManager.LoadScene("Players");
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("OverWorld");
+    }
+
+    public void BackTitle()
+    {
+        SceneManager.LoadScene("Title Screen");
+    }
+///////////////
+    // Amount of Players Screen
+    public void PlayNew()
+    {
+        SceneManager.LoadScene("OverWorld");
+    }
+
+    public void BackNewLoad()
+    {
+        SceneManager.LoadScene("NewLoad Game");
+    }
+}
