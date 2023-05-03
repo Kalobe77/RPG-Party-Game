723b1d6092fc70804e244cbde7e4bd242b498193
astefan1644 <90490226+astefan1644@users.noreply.github.com>
2023-05-03 17:36:15 -0400

    The Items don't work if the players has none
    Heal, Boost, and trap work. I'm not sure how to be able to choose a dice roll.

--- RPG Party Game/Assets/Scripts/ItemScript.cs
+++ RPG Party Game/Assets/Scripts/ItemScript.cs
@@ -1,32 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
 
 public class ItemScript : MonoBehaviour
 {
     public int[] itemTypes = {};
+    //public bool turn;
 
     // Takes in input script utilizing player tag
     public InputScript inputScript;
     public InputScript inputScript2;
+    public InventoryScript inventory1;
+    public InventoryScript inventory2;
 
     // Start is called before the first frame update
-    void Start()
+    void Start() 
     {
         // Links the input script using the tag
         inputScript = GameObject.FindGameObjectWithTag("Player 1").GetComponent<InputScript>();
         inputScript2 = GameObject.FindGameObjectWithTag("Player 2").GetComponent<InputScript>();
+
+        inventory1 = GameObject.FindGameObjectWithTag("Player1Inventory").GetComponent<InventoryScript>();
+        inventory2 = GameObject.FindGameObjectWithTag("Player2Inventory").GetComponent<InventoryScript>();
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() 
     {
         
     }
 
     public void Heal()
     {
-        if(inputScript.isTurn)
+        //turn = inputScript.isTurn;
+
+        if(inputScript.isTurn && (inventory1.itemStorage[0] != 0))
         {
             if (inputScript.remaininghp + 5 > inputScript.maxhp)
             {
@@ -39,7 +49,7 @@ public class ItemScript : MonoBehaviour
             }
         }
 
-        else if(inputScript2.isTurn)
+        else if(inputScript2.isTurn && (inventory2.itemStorage[0] != 0))
         {
             if (inputScript2.remaininghp + 5 > inputScript2.maxhp)
             {
@@ -55,13 +65,13 @@ public class ItemScript : MonoBehaviour
 //
     public void Trap()
     {
-        if(inputScript.isTurn)
+        if(inputScript.isTurn && (inventory1.itemStorage[1] != 0))
         {
             inputScript.spaceAssign[inputScript.node] = 4;
             inputScript2.spaceAssign[inputScript.node] = 4;
         }
 
-        else if(inputScript2.isTurn)
+        else if(inputScript2.isTurn && (inventory2.itemStorage[1] != 0))
         {
             inputScript.spaceAssign[inputScript2.node] = 4;
             inputScript2.spaceAssign[inputScript2.node] = 4;
@@ -103,7 +113,7 @@ public class ItemScript : MonoBehaviour
 
     public void Boost()
     {
-        if(inputScript.isTurn)
+        if(inputScript.isTurn && (inventory1.itemStorage[3] != 0))
         {
             inputScript.atk = inputScript.atk + 1;
             inputScript.def = inputScript.def + 1;
@@ -112,7 +122,7 @@ public class ItemScript : MonoBehaviour
             inputScript.spd = inputScript.spd + 1;
         }
 
-        else if(inputScript2.isTurn)
+        else if(inputScript2.isTurn && (inventory2.itemStorage[3] != 0))
         {
             inputScript2.atk = inputScript2.atk + 1;
             inputScript2.def = inputScript2.def + 1;
